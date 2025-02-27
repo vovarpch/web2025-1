d4eeed58a4cdd7b5d73a73524f08eb368fa9f96a
Vova <101842297+vovarpch@users.noreply.github.com>
2025-02-27 16:04:14 +0200

    Update lab.js

--- lab.js
+++ lab.js
@@ -4,5 +4,5 @@ try {
     const githubUsername = execSync("git config --get user.name").toString().trim();
     console.log(`Hello, ${githubUsername}`);
 } catch (error) {
-    console.error("Не вдалося отримати ім'я користувача GitHub. Налаштуйте його через `git config --global user.name`");
+    console.error("Не вдалося отримати ім'я користувача GitHub.");
 }
